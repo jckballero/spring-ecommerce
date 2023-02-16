223961a862d1ffceee83ea245eea3f00057a0850
Jckballero <jean.caballero29@hotmail.com>
2023-02-15 22:53:20 -0500

    creacion de clases model, mapeo de clases tablas con JPA

--- src/main/java/com/curso/ecommerce/model/DetalleOrden.java
+++ src/main/java/com/curso/ecommerce/model/DetalleOrden.java
@@ -1,13 +1,24 @@
 package com.curso.ecommerce.model;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "detalles")
 public class DetalleOrden {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String nombre;
     private double cantidad;
     private double precio;
     private double total;
 
+    @OneToOne
+    private Orden orden;
+
+    @ManyToOne
+    private Producto producto;
     public DetalleOrden() {
     }
 
@@ -57,6 +68,22 @@ public class DetalleOrden {
 
     public void setTotal(double total) {
         this.total = total;
+    }
+
+    public Orden getOrden() {
+        return orden;
+    }
+
+    public void setOrden(Orden orden) {
+        this.orden = orden;
+    }
+
+    public Producto getProducto() {
+        return producto;
+    }
+
+    public void setProducto(Producto producto) {
+        this.producto = producto;
     }
 
     @Override
--- src/main/java/com/curso/ecommerce/model/Usuario.java
+++ src/main/java/com/curso/ecommerce/model/Usuario.java
@@ -1,7 +1,14 @@
 package com.curso.ecommerce.model;
 
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "usuarios")
 public class Usuario {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String nombre;
     private String username;
@@ -10,6 +17,12 @@ public class Usuario {
     private String telefono;
     private String tipo;
     private String password;
+
+    @OneToMany(mappedBy = "usuario")
+    private List<Producto> productos;
+
+    @OneToMany(mappedBy = "usuario")
+    private List<Orden> ordenes;
 
     public Usuario() {
     }
@@ -87,6 +100,14 @@ public class Usuario {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public List<Producto> getProductos() {
+        return productos;
+    }
+
+    public void setProductos(List<Producto> productos) {
+        this.productos = productos;
     }
 
     @Override
